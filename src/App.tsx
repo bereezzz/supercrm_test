5f7908ac2aef9d4c912ef0487b2991571d3bdc7d
DoNegative <aptemka_4@mail.ru>
2023-09-02 01:42:48 +0300

    result

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header/Header';
+import BlockCards from './components/blockCards/BlockCards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header></Header>
+      <div style={{width:"100%", height:"100%", display:"flex", justifyContent:"center", alignItems:'center'}}>
+        <BlockCards></BlockCards>
+      </div>
     </div>
   );
 }
